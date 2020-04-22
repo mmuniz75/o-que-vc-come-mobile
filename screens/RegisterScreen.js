33ade1440a794cf22eb1fb051bf9577e10d0094c
mmuniz75 <mmunizs1975@gmail.com>
2020-04-22 17:12:24 -0300

    botao de salvar

--- screens/RegisterScreen.js
+++ screens/RegisterScreen.js
@@ -1,26 +1,32 @@
-import React, {useState} from 'react';
-import {View, Text, StyleSheet, FlatList, Switch, TouchableOpacity, TextInput, Platform} from 'react-native';
-import {Ionicons} from '@expo/vector-icons';
+import React, { useState } from 'react';
+import { View, Text, StyleSheet, FlatList, Switch, TouchableOpacity, TextInput, Platform } from 'react-native';
+import { Ionicons } from '@expo/vector-icons';
 import Colors from '../constants/Colors';
+import { HeaderButtons, Item } from 'react-navigation-header-buttons';
+import HeaderButton from '../components/UI/HeaderButton';
 
-import {CHEMICALS} from '../services/chemicalService'
+import { CHEMICALS } from '../services/chemicalService'
+
+const save = (navigation) => {
+    console.log('saved !');
+    navigation.navigate('Main');
+}
 
 
-
-const setCheck = (index,value,chemicalsCheck, setChemicallCheck) => {
+const setCheck = (index, value, chemicalsCheck, setChemicallCheck) => {
     const updatedChecks = [...chemicalsCheck];
     updatedChecks[index] = value;
     setChemicallCheck(updatedChecks);
 }
 
-const renderChemical = (index,item,chemicalsCheck, setChemicallCheck) => {
-     return <View style={styles.items}>
-            <Text style={styles.item}>{item.name}</Text>
-            <Switch value={chemicalsCheck} 
-                    thumbColor={Platform.OS === 'android' ? Colors.primaryColor : ''}
-                    trackColor={ {true : Colors.primaryColor}}
-                    onValueChange={newValue => setChemicallCheck(newValue)}/>
-           </View>
+const renderChemical = (index, item, chemicalsCheck, setChemicallCheck) => {
+    return <View style={styles.items}>
+        <Text style={styles.item}>{item.name}</Text>
+        <Switch value={chemicalsCheck}
+            thumbColor={Platform.OS === 'android' ? Colors.primaryColor : ''}
+            trackColor={{ true: Colors.primaryColor }}
+            onValueChange={newValue => setChemicallCheck(newValue)} />
+    </View>
 }
 
 const RegisterScreen = props => {
@@ -28,7 +34,7 @@ const RegisterScreen = props => {
     const [barcode, setBarcode] = useState('');
     const [brand, setBrand] = useState('');
     const [food, setFood] = useState('');
-    
+
     return (
         <View style={styles.screen}>
             <Text style={styles.text}>Cadastre os produtos quimicos da sua marca ou alimento.</Text>
@@ -42,8 +48,8 @@ const RegisterScreen = props => {
                             onChangeText={text => setBarcode(text)}
                         />
                         <TouchableOpacity>
-                            <Ionicons 
-                                name={Platform.OS === 'android' ? 'md-barcode' : 'ios-barcode'}  
+                            <Ionicons
+                                name={Platform.OS === 'android' ? 'md-barcode' : 'ios-barcode'}
                                 size={23}
                             />
                         </TouchableOpacity>
@@ -58,14 +64,14 @@ const RegisterScreen = props => {
                             onChangeText={text => setFood(text)}
                         />
                         <TouchableOpacity>
-                            <Ionicons 
-                                name={Platform.OS === 'android' ? 'md-add' : 'ios-add'}  
+                            <Ionicons
+                                name={Platform.OS === 'android' ? 'md-add' : 'ios-add'}
                                 size={23}
                             />
                         </TouchableOpacity>
                     </View>
                 </View>
-            
+
                 <View style={styles.formControl}>
                     <Text style={styles.label}>Escolha a Marca</Text>
                     <View style={styles.textContainer}>
@@ -75,50 +81,70 @@ const RegisterScreen = props => {
                             onChangeText={text => setBrand(text)}
                         />
                         <TouchableOpacity>
-                            <Ionicons 
-                                name={Platform.OS === 'android' ? 'md-add' : 'ios-add'}  
+                            <Ionicons
+                                name={Platform.OS === 'android' ? 'md-add' : 'ios-add'}
                                 size={23}
                             />
                         </TouchableOpacity>
                     </View>
                 </View>
-                
+
             </View>
-            <FlatList  style={styles.list} data={CHEMICALS} renderItem={ ({item,index}) => renderChemical(index,item,chemicalsCheck, setChemicallCheck)} />
+            <FlatList style={styles.list}
+                      data={CHEMICALS} 
+                      keyExtractor={item => item.id}
+                      renderItem={({ item, index }) => renderChemical(index, item, chemicalsCheck, setChemicallCheck)} />
         </View>
     )
 }
 
+RegisterScreen.navigationOptions = navData => {
+    return {
+        headerRight: (
+            <HeaderButtons HeaderButtonComponent={HeaderButton}>
+                <Item
+                    title="Save"
+                    iconName={
+                        Platform.OS === 'android' ? 'md-checkmark' : 'ios-checkmark'
+                    }
+                    onPress={() => save(navData.navigation)}
+                />
+            </HeaderButtons>
+        )
+    };
+};
+
+
 const styles = StyleSheet.create({
-    screen : {
-       flex : 1,
-       justifyContent : 'center',
-       alignItems : 'center',
+    screen: {
+        flex: 1,
+        justifyContent: 'center',
+        alignItems: 'center',
     },
-    item : {
-        fontFamily : 'open-sans',
-        fontSize : 16,
-        marginHorizontal : 15
+    item: {
+        fontFamily: 'open-sans',
+        fontSize: 16,
+        marginHorizontal: 15
     },
-    items : {
-        flexDirection : 'row',
-        alignItems : 'center',
-        flex : 1,
-        marginVertical : 15,
-        marginRight : 20,
-        justifyContent : "space-between"
+    items: {
+        flexDirection: 'row',
+        alignItems: 'center',
+        flex: 1,
+        marginVertical: 15,
+        marginRight: 20,
+        justifyContent: "space-between"
     },
-    list : {
+    list: {
         width: '100%',
     },
-    text : {
-        marginVertical : 10,
-        fontFamily : 'open-sans-bold',
+    text: {
+        marginVertical: 10,
+        fontFamily: 'open-sans-bold',
         fontSize: 16,
-        textAlign : 'center'
+        textAlign: 'center'
     },
-    textContainer : {
-        flexDirection : 'row'
+    textContainer: {
+        flexDirection: 'row'
     },
     form: {
         margin: 20,
@@ -126,7 +152,7 @@ const styles = StyleSheet.create({
     },
     formControl: {
         width: '100%',
-        marginBottom : 10
+        marginBottom: 10
     },
     label: {
         fontFamily: 'open-sans-bold',
@@ -137,7 +163,7 @@ const styles = StyleSheet.create({
         paddingVertical: 5,
         borderBottomColor: '#ccc',
         borderBottomWidth: 1,
-        width : '90%'
+        width: '90%'
     }
 });
 
